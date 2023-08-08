97463f68c02f5ba4ca986048ec5e02acd772c25a
PikeDeveloper <cabelloenrique@hotmail.com>
2023-08-08 00:10:17 -0500

    intento 1

---
+++ _static/server/flight-server-css-manifest.js
@@ -0,0 +1,29 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/pc/Documentos/GitHub/next-php/next-front/src/app/layout.js": [
+      "/home/pc/Documentos/GitHub/next-php/next-front/node_modules/.pnpm/@next+font@13.4.4/node_modules/@next/font/google/target.css?{\"path\":\"src/app/layout.js\",\"import\":\"Poppins\",\"arguments\":[{\"weight\":[\"400\",\"700\"],\"subsets\":[\"latin\"]}],\"variableName\":\"font\"}",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/app/globals.css",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/app/page.module.css"
+    ],
+    "/home/pc/Documentos/GitHub/next-php/next-front/src/app/clients/page.js": [
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/components/filter/filter.module.css",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/components/add_new_client/clients.module.css",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/app/clients/clients.module.css",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/components/square_image/SquareImage.module.css"
+    ],
+    "/home/pc/Documentos/GitHub/next-php/next-front/src/app/clients/layout.js": [
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/app/clients/clients.module.css"
+    ]
+  },
+  "cssModules": {
+    "/home/pc/Documentos/GitHub/next-php/next-front/src/app/clients/page": [
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/app/clients/clients.module.css",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/app/globals.css",
+      "/home/pc/Documentos/GitHub/next-php/next-front/node_modules/.pnpm/@next+font@13.4.4/node_modules/@next/font/google/target.css?{\"path\":\"src/app/layout.js\",\"import\":\"Poppins\",\"arguments\":[{\"weight\":[\"400\",\"700\"],\"subsets\":[\"latin\"]}],\"variableName\":\"font\"}",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/app/page.module.css",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/components/add_new_client/clients.module.css",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/components/filter/filter.module.css",
+      "/home/pc/Documentos/GitHub/next-php/next-front/src/components/square_image/SquareImage.module.css"
+    ]
+  }
+}
